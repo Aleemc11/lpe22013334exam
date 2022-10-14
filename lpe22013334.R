035fe0105e720d54918680776976a02581df50c4
Ale <amoncal21@gmail.com>
2022-10-14 12:18:15 +0200

    Actualización examen

--- lpe22013334.R
+++ lpe22013334.R
@@ -1,15 +1,31 @@
-# Parte Práctica Alejandra Montes de Oca Calero - 22013334
+# Parte Práctica LPE Alejandra Montes de Oca Calero - 22013334
+# Fecha 14 oct 2022
+install.packages (c("tidyverse", "httr","janitor"))
+library(pacman);pacman::p_load(tidyverse,httr,janitor)
 
+# Importamos el dataset con el nombre deseado
 library(readr)
-catastro_sample <- read_csv("catastro-sample.csv")
+catastro_sample <- read_csv("lpe22013334/lpe22013334exam/catastro-sample.csv")
 View(catastro_sample)
 
+# 4.Limpiar el dataset: 
 
-# cd /Users/MiUsuarioEnMac/Desktop/github_folder
-# git clone https://github.com/juancadh/basicoding.git
-# cd basicoding
-# git status
-# git add "prueba.txt"
-# git commit -m "Enviando el archivo de prueba"
-#git remote -v
-# git push origin master
+janitor::clean_names(catastro_sample)%>%glimpse() 
+locale()
+catastro <-catastro_sample %>% janitor::clean_names()%>% type_convert(locale = locale(decimal_mark = ','))
+
+
+# 5
+
+#df_low <- df_final%>%mutate(expensive = !rotulo %in% c("CEPSA", "REPSOL", "BP", "SHELL")) # ! para encontrar las baratas (lo contrario a las caras)
+
+## Cual precio medio de gasoleo en las CCAA
+#df_low %>% select(precio_gasoleo_a, idccaa, rotulo, expensive) %>% drop_na() %>% group_by(idccaa, expensive) %>% summarise(mean(precio_gasoleo_a)) %>% view()
+#df_low %>% glimpse()
+
+
+write.csv(catastro,"C:\\Users\\aleemc11\\lpe22013334\\lpe22013334exam\\catastro.csv", row.names = FALSE)
+
+
+
+
