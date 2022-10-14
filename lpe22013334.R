bf08b45fbd5ba9c447afb1e6c16f41e35b16ffe6
Ale <amoncal21@gmail.com>
2022-10-14 11:23:45 +0200

    Prueba de subida examen parte práctica

---
+++ lpe22013334.R
@@ -0,0 +1,15 @@
+# Parte Práctica Alejandra Montes de Oca Calero - 22013334
+
+library(readr)
+catastro_sample <- read_csv("catastro-sample.csv")
+View(catastro_sample)
+
+
+# cd /Users/MiUsuarioEnMac/Desktop/github_folder
+# git clone https://github.com/juancadh/basicoding.git
+# cd basicoding
+# git status
+# git add "prueba.txt"
+# git commit -m "Enviando el archivo de prueba"
+#git remote -v
+# git push origin master
